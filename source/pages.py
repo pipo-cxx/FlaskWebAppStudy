f577efedbd1c9854ae57425f499f11068e9153c0
pipo.cxx <77166981+pipo-cxx@users.noreply.github.com>
2025-02-14 12:07:14 +0300

    Added redirect to home from default

--- source/pages.py
+++ source/pages.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect
 
 bp = Blueprint("pages", __name__)
 
@@ -6,6 +6,10 @@ bp = Blueprint("pages", __name__)
 ## pages is the name of bp, __name__ is for importing to __init__
 
 @bp.route('/')
+def index():
+    return redirect("/home", code=302)
+
+@bp.route('/home')
 def home():
     return render_template("pages/home.html")
 
